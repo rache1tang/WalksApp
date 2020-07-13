27e3047218ed1d92b4571bae90ca424b1fa6c119
Rachel Tang <rache1tang@fb.com>
2020-07-13 12:36:07 -0700

    created bottom navigation view and skeleton for each fragment

--- app/src/main/java/com/example/walksapp/MainActivity.java
+++ app/src/main/java/com/example/walksapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.walksapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.walksapp.fragments.HomeFragment;
+import com.example.walksapp.fragments.ProfileFragment;
+import com.example.walksapp.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.actionHome:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.actionProfile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.actionSearch:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.actionFavorites:
+                    default:
+                        // TODO: change to favorites when you make the favorites page
+                        fragment = new HomeFragment();
+
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        // set default selection
+        bottomNavigation.setSelectedItemId(R.id.actionHome);
+
     }
 }
