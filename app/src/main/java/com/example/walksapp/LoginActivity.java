2ecf86c6b83cafd6cf5f46ab1deb6d71d58a1bfd
Rachel Tang <rache1tang@fb.com>
2020-07-13 12:08:26 -0700

    implemented signup page and sign in page

---
+++ app/src/main/java/com/example/walksapp/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.example.walksapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    EditText etLoginUsername;
+    EditText etLoginPassword;
+    Button btnLogin;
+    Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // if there is someone signed in already go directly to main activity
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        etLoginUsername = findViewById(R.id.etLoginUsername);
+        etLoginPassword = findViewById(R.id.etLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etLoginUsername.getText().toString();
+                String password = etLoginPassword.getText().toString();
+
+                loginUser(username, password);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    //TODO: notify user that there is an issue with login
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish(); // finish this activity -- once you are logged in, you are good to go
+    }
+}
---
+++ app/src/main/java/com/example/walksapp/SignUpActivity.java
@@ -0,0 +1,109 @@
+package com.example.walksapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+import java.util.List;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignUpActivity";
+
+    EditText etUsername;
+    EditText etPassword;
+    EditText etPassRepeat;
+    Button btnMakeAcc;
+    EditText etName;
+    boolean userExists;
+    EditText etEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        etUsername = findViewById(R.id.etSignupUsername);
+        etPassword = findViewById(R.id.etSignupPassword);
+        etPassRepeat = findViewById(R.id.etPasswordRepeat);
+        btnMakeAcc = findViewById(R.id.btnMakeAccount);
+        etName = findViewById(R.id.etSignupName);
+        etEmail = findViewById(R.id.etSignupEmail);
+
+        btnMakeAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String passRepeat = etPassRepeat.getText().toString();
+                String name = etName.getText().toString();
+                String email = etEmail.getText().toString();
+
+                makeAccount(username, name, password, passRepeat, email);
+            }
+        });
+    }
+
+    private void makeAccount(String username, String name, String password, String passRepeat, String email) {
+        if (username.isEmpty() || password.isEmpty() || passRepeat.isEmpty() || name.isEmpty() || email.isEmpty()) {
+            Toast.makeText(SignUpActivity.this, "All fields must be filled out", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!password.equals(passRepeat)) {
+            Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (userExists(username)) {
+            Toast.makeText(SignUpActivity.this, "Username is already taken", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ParseUser user = new ParseUser();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setUsername(username);
+        user.put("name", name);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "error saving new user", e);
+                    return;
+                }
+                Toast.makeText(getApplicationContext(), "Sign in to use app!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+    }
+
+    private boolean userExists(String username) {
+        ParseQuery<ParseUser> query = ParseQuery.getQuery(ParseUser.class);
+        query.whereEqualTo("username", username);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "error checking username");
+                    return;
+                }
+                userExists = objects.size() > 0;
+            }
+        });
+        return userExists;
+    }
+}
